540ee71ad54e0199d9044d2d5c875d12d5e15a2a
Raymiray <ritsiscream11@yandex.ru>
2024-06-21 15:01:26 +0300

    add task about coins

--- Sem2/Task5.py
+++ Sem2/Task5.py
@@ -24,3 +24,42 @@
 # ООООРРРРОРОРРРРРРРРООРОРОРРРРРРРРРРРРРРРРРРРРРРРРРРРРРРР
 # Sample Output 3:
 # 31
+
+# Первое решение
+n = 'ООООООРРРОРОРРРРРРР'
+o = 0
+om = 0
+p = 0
+pm = 0
+for i in n:
+    if i == 'О':
+        o += 1
+        om = o
+        p = 0
+    elif i == 'Р':
+        p += 1
+        pm = p
+        o = 0
+
+print(max(om, pm))
+
+
+# Второе решение
+import random
+
+num_try = int(input("Введите количеств бросков монеток = "))
+
+string = ""
+for i in range(num_try):
+    string += random.choice(["Р","О"])
+
+print(string)
+
+max = 0
+count = 1
+i = 0
+
+while "Р" * i in string:
+    i += 1
+print(i - 1)
+print("Р" not in ["Р","О"])
